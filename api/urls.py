370c1c9bd1636359b72dd8f5f17b58b06e848447
abdelgayoom <abdu86069@gmail.com>
2020-05-20 02:45:52 +0200

    add urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+
+    path('post/', views.PostsListView.as_view()),
+    path('post/<int:pk>/', views.PostsDetailView.as_view()),
+    path('post/<int:pk>/comments/', views.CommentsListView.as_view()),
 
 ]
