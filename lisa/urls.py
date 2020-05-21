78e640d649ecdf97028c53ae39f960128f3e5cf8
abdelgayoom <abdu86069@gmail.com>
2020-05-21 16:11:06 +0200

    disable no api login

--- lisa/urls.py
+++ lisa/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('account/', include('allauth.urls')),
+    #path('account/', include('allauth.urls')),
     path('api/v1/auth/', include('rest_auth.urls')),
     path('api/v1/auth/register/', include('rest_auth.registration.urls')),
     path('api/v1/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api/v1/rest_auth/', include('rest_framework.urls'))
 ]
