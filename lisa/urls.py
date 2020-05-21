8f4399de26b5084a5aeadc3c4cffd40b4fded0ab
abdelgayoom <abdu86069@gmail.com>
2020-05-21 13:05:09 +0200

    orgnize urls

--- lisa/urls.py
+++ lisa/urls.py
@@ -4,10 +4,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
-    path('auth/', include('rest_auth.urls')),
-    path('auth/register/', include('rest_auth.registration.urls')),
     path('account/', include('allauth.urls')),
+    path('api/v1/auth/', include('rest_auth.urls')),
+    path('api/v1/auth/register/', include('rest_auth.registration.urls')),
     path('api/v1/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
